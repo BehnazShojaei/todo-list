8e8a1c6417ea8d02a071912a5d34751ca988f182
Behnaz <behnaz.shojaei88@gmail.com>
2024-11-19 11:46:45 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+
+
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    "Learn about React",
+    "Meet a friend for lunch",
+    "Build a really cool todo app",
+  ]);
+
+  return (
+    <div>
+      <h1>My todo list</h1>
+      {todos.map((todo, index) => (
+        <TodoItem text={todo} key={index} />
+
+      ))}
+
+      <TodoForm />
+
+    </div>
+  );
+
+}
+
+export default App;
+
+
